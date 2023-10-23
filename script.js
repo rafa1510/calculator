df965716ec18bae97c7d142861873f67f3179b07
rafa1510 <rcestti78@gmail.com>
2023-10-23 03:29:42 -0400

    Make the calculator work
    Add event listeners to all the number buttons & operators, and
    initialize a chosenNumbers array and chosenOperator variable. Clicking
    equal's button will call the operate() function with the two numbers
    that will be stored in chosenNumbers, and the operator in chosenOperator
    
    Another way to operate is by clicking an operator twice, this will
    perform the operation then add in the result to the chosenNumbers array.
    This is so that the user can keep chaining operators and the calculator
    will do the operations.

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-// Adds event listeners to all numbers that calls display() and displays the chosen number
+// Display the chosen number
+let currentDisplay = "";
 let numberButtons = document.querySelectorAll(".numbers");
 numberButtons.forEach((number) => 
 {
-    number.addEventListener("click", () => display(number.textContent));
+    number.addEventListener("click", () => 
+    {
+        currentDisplay += number.textContent;
+        display(currentDisplay);
+    });
 })
+
+// Store last chosen operator in chosenOperator and add the currentDisplay to chosenNumbers array
+let operatorButtons = document.querySelectorAll(".operators");
+operatorButtons.forEach((operator) => 
+{
+    operator.addEventListener("click", () => 
+    {
+        // This is here so that we don't push no numbers into the chosenNumbers array
+        if (currentDisplay != "")
+        {
+            chosenNumbers.push(parseInt(currentDisplay));
+        }
+
+        // Perform operation if there was already an operator clicked & there are numbers in the chosenNumbers array
+        if (chosenOperator != "" && chosenNumbers.length == 2)
+        {
+            let result = operate(chosenNumbers[0], chosenNumbers[1], chosenOperator);
+            let roundedResult = Math.round(result * 100) / 100;
+            // Display result from calling operate
+            display(roundedResult);
+            // Clear currentDisplay, chosenNumbers, chosenOperator to make space for the next operation
+            currentDisplay = "";
+            chosenNumbers.splice(0, 2, parseInt(result));
+            chosenOperator = "";
+        }
+
+        chosenOperator = operator.textContent;
+
+        // Clear currentDisplay to make way for new numbers
+        currentDisplay = "";
+    })
+})
+
+function display(number)
+{
+    let displayBox = document.querySelector(".display");
+    displayBox.textContent = number.toString();
+}
 
 function add(numOne, numTwo)
 {
@@ -45,8 +88,27 @@ function operate(numOne, numTwo, operator)
     }
 }
 
-function display(number)
+// Array that stores the numbers we are going to operate on
+let chosenNumbers = [];
+// Variable holding user's last chosen operator
+let chosenOperator = "";
+
+// Call operate() function with two numbers and an operator once equal's button is pressed
+let equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => 
 {
-    let displayBox = document.querySelector(".display");
-    displayBox.textContent = number;
-}
+    // Makes sure that there are numbers in the chosenNumbers array
+    if (chosenNumbers.length != 0)
+    {
+        // Add in second number to chosenNumbers array, since it's not being added by clicking an operator
+        chosenNumbers.push(parseInt(currentDisplay));
+        let result = operate(chosenNumbers[0], chosenNumbers[1], chosenOperator);
+        let roundedResult = Math.round(result * 100) / 100;
+        // Display result from calling operate
+        display(roundedResult);
+        // Clear currentDisplay, chosenNumbers, chosenOperator to make space for the next operation
+        currentDisplay = "";
+        chosenNumbers.splice(0, 2, parseInt(result));
+        chosenOperator = "";
+    }
+})
